65a6bae67aed5100d7997b313a284e4e344ff593
caravanadestrucs <li_djoaquin@unca.edu.mx>
2026-04-29 20:19:58 -0600

    lol haber si sirve

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,5 +1,7 @@
 import api from './index';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const login = async (email, password) => {
   const response = await api.post('/auth/login', { email, password });
   return response.data;
